60a2f82a027eb8e17e11a2d91f5ad1e51e04ec09
Karthik Subramanian <karthik.s.sundaram@gmail.com>
2014-03-01 16:42:36 +0530

    added user into the session, changed config

--- default_config.js
+++ default_config.js
@@ -2,5 +2,6 @@ module.exports = {
   TWITTER_CONSUMER_KEY : "",
   TWITTER_CONSUMER_SECRET : "",
   ACCESS_TOKEN_KEY : "",
-  ACCESS_TOKEN_SECRET : ""
+  ACCESS_TOKEN_SECRET : "",
+  DB_HOSTNAME : "mongodb://localhost:27017/ftl"
 };
