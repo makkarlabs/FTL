aa58ccc9fd9698a18286bade002df7fee9139681
Abhinandan <abhiin1947@gmail.com>
2014-03-02 01:05:06 +0530

    streaming done

--- default_config.js
+++ default_config.js
@@ -1,7 +1,7 @@
 module.exports = {
   TWITTER_CONSUMER_KEY : "",
   TWITTER_CONSUMER_SECRET : "",
-  ACCESS_TOKEN_KEY : "",
-  ACCESS_TOKEN_SECRET : "",
+  TWITTER_ACCESS_TOKEN_KEY : "",
+  TWITTER_ACCESS_TOKEN_SECRET : "",
   DB_HOSTNAME : "mongodb://localhost:27017/ftl"
 };
